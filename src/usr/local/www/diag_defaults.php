e940f0825cd8dbb9a3f294f6fb17b75a954bc07a
TypeScriptKorea <jeawon139@gmail.com>
2018-02-21 08:51:11 +0900

    Update diag_defaults.php
    주석, 한글번역

--- src/usr/local/www/diag_defaults.php
+++ src/usr/local/www/diag_defaults.php
@@ -23,6 +23,11 @@
  * limitations under the License.
  */
 
+/*
+2018.02.21
+한글화 번역 시작
+*/
+
 ##|+PRIV
 ##|*IDENT=page-diagnostics-factorydefaults
 ##|*NAME=Diagnostics: Factory defaults
@@ -33,17 +38,17 @@
 
 require_once("guiconfig.inc");
 
-if ($_POST['Submit'] == " " . gettext("No") . " ") {
+if ($_POST['Submit'] == " " . gettext("아니오") . " ") {
 	header("Location: index.php");
 	exit;
 }
 
-$pgtitle = array(gettext("Diagnostics"), gettext("Factory Defaults"));
+$pgtitle = array(gettext("진단"), gettext("Factory Defaults"));
 include("head.inc");
 ?>
 
-<?php if ($_POST['Submit'] == " " . gettext("Yes") . " "):
-	print_info_box(gettext("The system has been reset to factory defaults and is now rebooting. This may take a few minutes, depending on the hardware."))?>
+<?php if ($_POST['Submit'] == " " . gettext("예") . " "):
+	print_info_box(gettext("시스템이 공장 초기화로 재설정되었으며 현재 재부팅 중입니다."))?>
 <pre>
 <?php
 	reset_factory_defaults();
@@ -53,30 +58,30 @@ include("head.inc");
 <?php else:?>
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h2 class="panel-title"><?=gettext("Factory Defaults Reset")?></h2>
+		<h2 class="panel-title"><?=gettext("공장 초기화 및 재설정")?></h2>
 	</div>
 	<div class="panel-body">
 		<div class="content">
 			<form action="diag_defaults.php" method="post">
-				<p><strong><?=gettext('Resetting the system to factory defaults will remove all user configuration and apply the following settings:')?></strong></p>
+				<p><strong><?=gettext('시스템을 공장 초기화로 재설정하면 모든 사용자 구성이 제거되고 다음과 같은 설정이 적용됩니다.:')?></strong></p>
 				<ul>
-					<li><?=gettext("Reset to factory defaults")?></li>
-					<li><?=gettext("LAN IP address will be reset to 192.168.1.1")?></li>
-					<li><?=gettext("System will be configured as a DHCP server on the default LAN interface")?></li>
-					<li><?=gettext("Reboot after changes are installed")?></li>
-					<li><?=gettext("WAN interface will be set to obtain an address automatically from a DHCP server")?></li>
-					<li><?=gettext("webConfigurator admin username will be reset to 'admin'")?></li>
-					<li><?=sprintf(gettext("webConfigurator admin password will be reset to '%s'"), $g['factory_shipped_password'])?></li>
+					<li><?=gettext("공장 초기화를 통한 시스템 재구성")?></li>
+					<li><?=gettext("LAN IP 주소가 192.168.1.1 로 리셋됩니다.")?></li>
+					<li><?=gettext("시스템이 기본 LAN인터페이스에서 DHCP서버로 구성됩니다.")?></li>
+					<li><?=gettext("변경사항 설치 후 재부팅됩니다.")?></li>
+					<li><?=gettext("WAN 인터페이스가 DHCP 서버에서 자동으로 주소를 가져오도록 설정됩니다.")?></li>
+					<li><?=gettext("webConfigurator admin 사용자 이름이 'admin'으로 리셋됩니다.")?></li>
+					<li><?=sprintf(gettext("webConfigurator admin 암호가 '%s'(으)로 리셋됩니다."), $g['factory_shipped_password'])?></li>
 				</ul>
 				<p><strong><?=gettext("Are you sure you want to proceed?")?></strong></p>
 				<p>
-					<button name="Submit" type="submit" class="btn btn-sm btn-danger" value=" <?=gettext("Yes")?> " title="<?=gettext("Perform a factory reset")?>">
+					<button name="Submit" type="submit" class="btn btn-sm btn-danger" value=" <?=gettext("예")?> " title="<?=gettext("공장 초기화 실행")?>">
 						<i class="fa fa-undo"></i>
-						<?=gettext("Factory Reset")?>
+						<?=gettext("공장 초기화")?>
 					</button>
-					<button name="Submit" type="submit" class="btn btn-sm btn-success" value=" <?=gettext("No")?> " title="<?=gettext("Return to the dashboard")?>">
+					<button name="Submit" type="submit" class="btn btn-sm btn-success" value=" <?=gettext("")?> " title="<?=gettext("대시보드로 돌아가기")?>">
 						<i class="fa fa-save"></i>
-						<?=gettext("Keep Configuration")?>
+						<?=gettext("구성 유지")?>
 					</button>
 				</p>
 			</form>

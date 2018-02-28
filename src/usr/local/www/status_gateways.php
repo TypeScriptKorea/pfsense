c8a7993cb4780e3b86d86c1f2ebc05fa3ec378b5
TypeScriptKorea <jeawon139@gmail.com>
2018-02-28 11:11:41 +0900

    Update status_gateways.php
    주석, 한글 번역 추가

--- src/usr/local/www/status_gateways.php
+++ src/usr/local/www/status_gateways.php
@@ -20,6 +20,11 @@
  * limitations under the License.
  */
 
+/*
+2018.02.28
+한글화 번역 시작
+*/
+
 ##|+PRIV
 ##|*IDENT=page-status-gateways
 ##|*NAME=Status: Gateways
@@ -45,21 +50,21 @@ include("head.inc");
 
 /* active tabs */
 $tab_array = array();
-$tab_array[] = array(gettext("Gateways"), true, "status_gateways.php");
-$tab_array[] = array(gettext("Gateway Groups"), false, "status_gateway_groups.php");
+$tab_array[] = array(gettext("게이트웨이"), true, "status_gateways.php");
+$tab_array[] = array(gettext("게이트웨이 "), false, "status_gateway_groups.php");
 display_top_tabs($tab_array);
 ?>
 <div class="panel panel-default">
-	<div class="panel-heading"><h2 class="panel-title"><?=gettext('Gateways')?></h2></div>
+	<div class="panel-heading"><h2 class="panel-title"><?=gettext('게이트웨이')?></h2></div>
 	<div class="panel-body">
 
 <div class="table-responsive">
 	<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable>
 		<thead>
 			<tr>
-				<th><?=gettext("Name"); ?></th>
-				<th><?=gettext("Gateway"); ?></th>
-				<th><?=gettext("Monitor"); ?></th>
+				<th><?=gettext("임재원"); ?></th>
+				<th><?=gettext("게이트웨이"); ?></th>
+				<th><?=gettext("모니터"); ?></th>
 				<th><?=gettext("RTT"); ?></th>
 				<th><?=gettext("RTTsd"); ?></th>
 				<th><?=gettext("Loss"); ?></th>
@@ -123,35 +128,35 @@ display_top_tabs($tab_array);
 				if ($gateways_status[$gname]) {
 					$status = $gateways_status[$gname];
 					if (stristr($status['status'], "force_down")) {
-						$online = gettext("Offline (forced)");
+						$online = gettext("오프라인 (forced)");
 						$bgcolor = "bg-danger";
 					} elseif (stristr($status['status'], "down")) {
-						$online = gettext("Offline");
+						$online = gettext("오프라인");
 						$bgcolor = "bg-danger";
 					} elseif (stristr($status['status'], "highloss")) {
-						$online = gettext("Danger, Packetloss") . ': ' . $status['loss'];
+						$online = gettext("위험, Packetloss") . ': ' . $status['loss'];
 						$bgcolor = "bg-danger";
 					} elseif (stristr($status['status'], "loss")) {
-						$online = gettext("Warning, Packetloss") . ': ' . $status['loss'];
+						$online = gettext("경고, Packetloss") . ': ' . $status['loss'];
 						$bgcolor = "bg-warning";
 					} elseif (stristr($status['status'], "highdelay")) {
-						$online = gettext("Danger, Latency") . ': ' . $status['delay'];
+						$online = gettext("위험, Latency") . ': ' . $status['delay'];
 						$bgcolor = "bg-danger";
 					} elseif (stristr($status['status'], "delay")) {
-						$online = gettext("Warning, Latency") . ': ' . $status['delay'];
+						$online = gettext("경고, Latency") . ': ' . $status['delay'];
 						$bgcolor = "bg-warning";
 					} elseif ($status['status'] == "none") {
 						if ($status['monitor_disable'] || ($status['monitorip'] == "none")) {
-							$online = gettext("Online (unmonitored)");
+							$online = gettext("온라인 (unmonitored)");
 						} else {
-							$online = gettext("Online");
+							$online = gettext("온라인");
 						}
 						$bgcolor = "bg-success";
 					}
 				} else if (isset($gateway['monitor_disable'])) {
 					// Note: return_gateways_status() always returns an array entry for all gateways,
 					//       so this "else if" never happens.
-						$online = gettext("Online (unmonitored)");
+						$online = gettext("온라인 (unmonitored)");
 						$bgcolor = "bg-success";
 				} else {
 					$online = gettext("Pending");
